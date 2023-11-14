b53f18012c1dc42bacd7361fde5b7ea2c84eeec6
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2023-11-14 08:54:39 +0700

    intent

--- app/src/main/java/com/example/retrofitapp/DescActivity.kt
+++ app/src/main/java/com/example/retrofitapp/DescActivity.kt
@@ -6,20 +6,27 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 
+// Activity responsible for displaying detailed information about a selected Rick and Morty character
 class DescActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_desc)
 
+        // Set the action bar title to the character's name received from the intent
         supportActionBar?.title = intent.getStringExtra("intent_Name")
 
+        // References to TextViews and ImageView in the layout
+        val NameTextView = findViewById<TextView>(R.id.Name)
         val speciesTextView = findViewById<TextView>(R.id.Sepcies)
         val statusTextView = findViewById<TextView>(R.id.status)
         val imageCharacterImageView = findViewById<ImageView>(R.id.image_character)
 
+        // Set the species and status information from the intent to the respective TextViews
+        NameTextView.text = intent.getStringExtra("intent_Name")
         speciesTextView.text = intent.getStringExtra("intent_Species")
         statusTextView.text = intent.getStringExtra("intent_status")
 
+        // Load the character image from the intent into the ImageView using Glide
         Glide.with(this)
             .load(intent.getStringExtra("intent_image"))
             .into(imageCharacterImageView)
--- app/src/main/java/com/example/retrofitapp/MainActivity.kt
+++ app/src/main/java/com/example/retrofitapp/MainActivity.kt
@@ -11,29 +11,37 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+// Main activity responsible for displaying a list of Rick and Morty characters
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Reference to the RecyclerView in the layout
         val rickRecyclerView = findViewById<RecyclerView>(R.id.ry_character)
 
+        // Make a network request using Retrofit to get Rick and Morty character data
         ApiConfig.getService().getRick().enqueue(object : Callback<ResponseRick> {
             override fun onResponse(call: Call<ResponseRick>, response: Response<ResponseRick>) {
                 if (response.isSuccessful) {
+                    // Retrieve the response body and extract the list of characters
                     val responseRick = response.body()
                     val dataRick = responseRick?.results
 
+                    // Create an adapter and pass the character data to it
                     val rickAdapter = RickAdapter(dataRick as List<ResultsItem>)
 
+                    // Set up the RecyclerView with a LinearLayoutManager and the adapter
                     rickRecyclerView.apply {
                         layoutManager = LinearLayoutManager(context)
                         setHasFixedSize(true)
                         adapter = rickAdapter
                     }
 
+                    // Set item click listener to handle click events on RecyclerView items
                     rickAdapter.setOnItemClickListener(object : RickAdapter.OnItemClickListener {
                         override fun onItemClick(result: ResultsItem) {
+                            // Open a new activity (DescActivity) with details of the clicked character
                             startActivity(
                                 Intent(applicationContext, DescActivity::class.java)
                                     .putExtra("intent_Name", result.name)
@@ -44,11 +52,14 @@ class MainActivity : AppCompatActivity() {
                         }
                     })
 
+                    // Notify the adapter that the data set has changed
                     rickAdapter.notifyDataSetChanged()
                 }
             }
 
+            // Handle failure in the network request
             override fun onFailure(call: Call<ResponseRick>, t: Throwable) {
+                // Display a toast message with the error information
                 Toast.makeText(applicationContext, t.localizedMessage, Toast.LENGTH_SHORT).show()
             }
         })
